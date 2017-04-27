f67ea79f72ed15b9ffccbcc62d4dc5d93e4ae2ac
Sonia <soniasophiaw@gmail.com>
2017-04-27 10:59:01 -0700

    Function Edit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,36 @@
-$(function () {
+// NYTimes API Key: 322b3247fdb14b38b38d51b8c82d9cad
+// http://api.nytimes.com/svc/topstories/v2/{section}.{response-format}?api-key={322b3247fdb14b38b38d51b8c82d9cad}
 
-  
+
+$(function () {
+  $('#sections').on('change', function () {
+    $('.stories').empty();
+
+    var section = this.value;
+    var url = 'http://api.nytimes.com/svc/topstories/v2/' + section + '.json?api-key=322b3247fdb14b38b38d51b8c82d9cad';
+
+
+    $.ajax({
+      method: 'GET',
+      url: url,
+
+  }).done(function (data) {
+    console.log(data);
+
+    $.each(data.results, function(index, value) {
+      if (value.multimedia.length !==0) {
+        $('.stories').append('<div class="story-cell"><img src="' + value.multimedia[2] + '">' + '<p><a href="' + value.url + value.abstract '">');
+      
+      }
+
+  }).fail(function (){
+    $('.stories').append('<p>Sorry, something went wrong.</p>')
+
+
+  })
+
+
+  });
+
 
 });
