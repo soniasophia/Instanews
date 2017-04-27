887e3285bb5de643bc7968a129f79bbfd517cc3f
Sonia <soniasophiaw@gmail.com>
2017-04-27 15:12:36 -0700

    jQuery Edit

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,23 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     rename = require('gulp-rename'),
     browserSync = require('browser-sync'),
-    eslint = require('gulp-eslint');
+    eslint = require('gulp-eslint'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cssnano = require('gulp-cssnano'),
+    prettyerror = require('gulp-prettyerror');
+
+
+gulp.task('sass', function () {
+  gulp.src('./SCSS/style.scss')
+    .pipe(prettyerror())
+    .pipe(sass())
+    .pipe(autoprefixer({ browsers: ['last 2 versions'] }))
+    .pipe(cssnano())
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('./build/css'))
+});
+
 
 
 gulp.task('scripts', ['eslint'], function () {
@@ -28,13 +44,14 @@ gulp.task('browser-sync', function () {
     }
   });
 
-gulp.watch(['./build/js/*.js', './CSS/style.css']).on('change', browserSync.reload);
+gulp.watch(['./build/js/*.js', './build/css/*.css']).on('change', browserSync.reload);
 });
 
 
 
 gulp.task('watch', function( ) {
   gulp.watch('./js/*.js', ['scripts'])
+  gulp.watch('./SCSS/*.scss', ['sass'])
 
 });
 
--- js/main.js
+++ js/main.js
@@ -1,13 +1,25 @@
-// NYTimes API Key: 322b3247fdb14b38b38d51b8c82d9cad
-// http://api.nytimes.com/svc/topstories/v2/{section}.{response-format}?api-key={322b3247fdb14b38b38d51b8c82d9cad}
-
 
 $(function () {
+  function hideLoader() {
+    $('.loader-gif').hide();
+  }
+  hideLoader();
+
+  console.log('hi')
+
+
+
   $('#sections').on('change', function () {
+    $('.loader-gif').show();
     $('.stories').empty();
 
     var section = this.value;
-    var url = 'http://api.nytimes.com/svc/topstories/v2/' + section + '.json?api-key=322b3247fdb14b38b38d51b8c82d9cad';
+    var storyString = '';
+    var runs = 0;
+    var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
+    url += '?' + $.param({
+    'api-key': "322b3247fdb14b38b38d51b8c82d9cad"
+});
 
 
     $.ajax({
@@ -16,12 +28,16 @@ $(function () {
 
   }).done(function (data) {
     console.log(data);
+    hideLoader();
 
     $.each(data.results, function(index, value) {
-      if (value.multimedia.length !==0) {
-        $('.stories').append('<div class="story-cell"><img src="' + value.multimedia[2] + '">' + '<p><a href="' + value.url + value.abstract '">');
+      if (value.multimedia.length >=5 && runs < 12 ) {
+        storyString += '<div class="story-cell"><img src="' + value.multimedia[4].url + '" class="story-image"><div class="story-link"><a href="' + value.url + '" target="_blank"><p class="story-abstract">' + value.abstract + '</a></p></div></div>';
       
-      }
+      runs++;
+    }
+    
+    $('.stories').append(storyString);
 
   }).fail(function (){
     $('.stories').append('<p>Sorry, something went wrong.</p>')
@@ -29,8 +45,9 @@ $(function () {
 
   })
 
-
   });
 
+
+});
 
 });
