c571a0ae05b23ea09eaaf4b3b8443d852654935e
Sonia <soniasophiaw@gmail.com>
2017-04-27 16:04:42 -0700

    jQuery Edit

--- js/main.js
+++ js/main.js
@@ -5,21 +5,19 @@ $(function () {
   }
   hideLoader();
 
-  console.log('hi')
-
-
 
   $('#sections').on('change', function () {
     $('.loader-gif').show();
     $('.stories').empty();
 
     var section = this.value;
+    console.log(section);
     var storyString = '';
     var runs = 0;
-    var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
+    var url = "https://api.nytimes.com/svc/topstories/v2/" + section + ".json";
     url += '?' + $.param({
     'api-key': "322b3247fdb14b38b38d51b8c82d9cad"
-});
+  });
 
 
     $.ajax({
@@ -32,22 +30,17 @@ $(function () {
 
     $.each(data.results, function(index, value) {
       if (value.multimedia.length >=5 && runs < 12 ) {
-        storyString += '<div class="story-cell"><img src="' + value.multimedia[4].url + '" class="story-image"><div class="story-link"><a href="' + value.url + '" target="_blank"><p class="story-abstract">' + value.abstract + '</a></p></div></div>';
-      
-      runs++;
+        storyString+= '<div class="story-cell"><img src="' + value.multimedia[4].url + '" class="story-image"><div class="story-link"><a href="' + value.url + '" target="_blank"><p class="story-abstract">' + value.abstract + '</a></p></div></div>';
+        runs++;
     }
     
-    $('.stories').append(storyString);
-
-  }).fail(function (){
-    $('.stories').append('<p>Sorry, something went wrong.</p>')
-
-
   })
+  $('.stories').append(storyString);
+  }).fail(function(){
+    $('.stories').append("<p>Sorry, something went wrong.</p>");
+
+    });
 
   });
-
-
-});
 
 });
